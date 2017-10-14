edbdf181e572d5f65c492a3b6a2a018968ae3460
Methuz Kaewsaikao <methuz.k@gmail.com>
2017-10-14 23:18:13 +0700

    Init

---
+++ block.go
@@ -0,0 +1,30 @@
+package main
+
+import "strconv"
+import "crypto/sha256"
+import "time"
+import "bytes"
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
